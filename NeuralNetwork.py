13a159674a6b50bc4fb76e306b3ce17e9b482cee
Dan Sirbu <dsirb066@gmail.com>
2017-03-15 14:11:06 -0400

    Added tanh activation function

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,16 +2,14 @@ import numpy as np
 
 
 class NeuralNetwork(object):
-    def __init__(self, sizes, activationFunction, activationFunctionPrime):
+    def __init__(self, sizes, activationFunction):
         # sizes = [input_size, middle_size, output_size]
 
         self.num_layers = len(sizes)
         self.num_hidden_layers = self.num_layers - 2
         self.n = sizes[0]
         self.sizes = sizes
-        # negative size = array.length - 1
-        self.vSigmoid = np.vectorize(activationFunction)
-        self.vSigmoidPrime = np.vectorize(activationFunctionPrime)
+        self.activationFunction = activationFunction
 
         self.weights = []
         for i in range(1, self.num_layers):
@@ -28,14 +26,14 @@ class NeuralNetwork(object):
         for i in range(self.num_layers - 1):
             weights = self.weights[i]
             z = np.dot(a, weights)
-            a = self.vSigmoid(z)
+            a = self.activationFunction(z)
 
             zs.append(z)
             activations.append(a)
 
         return zs, activations
 
-    def backprop(self, x, y, learningRate=1):
+    def backprop(self, x, y, learningRate=0.5):
         """
         :param x: input(s) in the form of a column vector (m x n)
         :param y: Expected output in the form of column vector (m x n)
@@ -49,11 +47,12 @@ class NeuralNetwork(object):
         deltas = np.empty(self.num_layers - 1, dtype=object)
 
         # last layer delta
-        deltas[-1] = np.multiply(-(y - yHat), self.vSigmoidPrime(zs[-1]))
+        deltas[-1] = np.multiply(-(y - yHat), self.activationFunction(zs[-1], derivative=True))
 
         # all layers delta except last one
         for layer in range(self.num_hidden_layers - 1, -1, -1):
-            deltas[layer] = np.multiply(np.dot(deltas[layer + 1], self.weights[layer + 1].T), self.vSigmoidPrime(zs[layer + 1]))
+            deltas[layer] = np.multiply(np.dot(deltas[layer + 1], self.weights[layer + 1].T),
+                                        self.activationFunction(zs[layer + 1], derivative=True))
 
         # weights updates (deltas * activation of previous layer)
         for layer in range(self.num_hidden_layers, -1, -1):
@@ -65,26 +64,32 @@ class NeuralNetwork(object):
         return J
 
 
+def sigmoid(z, derivative=False):
+    s = 1 / (1 + np.exp(-z))
 
-def sigmoidPrime(z):
-    return sigmoid(z) * (1 - sigmoid(z))
+    if derivative:
+        return s * (1 - s)
+    return s
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+def tanh(z, derivative=False): # doesn't work properly?
+    s = 2 / (1 + np.exp(-2*z)) - 1
 
+    if derivative:
+        return 1 - s**2
+    return s
 
 class Trainer(object):
     np.random.seed(1)
-    nn = NeuralNetwork([2, 3, 1], sigmoid, sigmoidPrime)
+    nn = NeuralNetwork([2, 3, 1], tanh)
 
     X = np.array(([1, 1], [0, 1], [1, 0], [0, 0]), dtype=float)
-    y = np.array(([1], [0], [0], [0]), dtype=float)
+    y = np.array(([0], [1], [1], [0]), dtype=float)
 
-    for _ in range(1, 100):
-        nn.backprop(X, y)
+    for _ in range(1, 200):
+        nn.backprop(X, y, learningRate=0.2)
         print(nn.costFunction(y, nn.feedforward(X)[-1][-1]))
 
-#assert nn.feedforward(np.array([1, 1]))[-1][-1] > 0.5
-#assert nn.feedforward(np.array([1, 0]))[-1][-1] < 0.5
-#assert nn.feedforward(np.array([0, 1]))[-1][-1] < 0.5
-#assert nn.feedforward(np.array([0, 0]))[-1][-1] < 0.5
+# assert nn.feedforward(np.array([1, 1]))[-1][-1] > 0.5
+# assert nn.feedforward(np.array([1, 0]))[-1][-1] < 0.5
+# assert nn.feedforward(np.array([0, 1]))[-1][-1] < 0.5
+# assert nn.feedforward(np.array([0, 0]))[-1][-1] < 0.5
